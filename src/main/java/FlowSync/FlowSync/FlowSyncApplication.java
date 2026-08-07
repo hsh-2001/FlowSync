a4df133ba708efa18e52551d02048629280ffa0c
senghong <senghong.learning@gmail.com>
2026-08-07 16:31:13 +0700

    Validate CORS

--- src/main/java/FlowSync/FlowSync/FlowSyncApplication.java
+++ src/main/java/FlowSync/FlowSync/FlowSyncApplication.java
@@ -1,7 +1,11 @@
 package FlowSync.FlowSync;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FlowSyncApplication {
@@ -10,4 +14,24 @@ public class FlowSyncApplication {
 		SpringApplication.run(FlowSyncApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(
+                    @NonNull CorsRegistry registry
+			) {
+
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods(
+								"GET",
+								"POST"
+						)
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
